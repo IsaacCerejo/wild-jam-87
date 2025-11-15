d08220ec12c1532a4481b6c0c8df3a68dbafd66d
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-11-15 18:59:42 +0000

    refactor: pause menu

--- scripts/start_menu/pause.gd
+++ scripts/start_menu/pause.gd
@@ -1,18 +1,14 @@
 extends Control
 
-@export var options_menu: Control
-@export var how_to_play_menu: Control
-
-@export var resume_button: Button
-@export var options_button: Button
-@export var how_to_play_button: Button
+@onready var options_menu: Control = %OptionsMenu
+@onready var how_to_play_menu: Control = %HowToPlayMenu
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Pause"):
-		open_close_pause()
-		
-func open_close_pause():
-	
+		_open_close_pause()
+
+# Private functions
+func _open_close_pause():
 	visible = !visible
 	if visible:
 		get_tree().paused = true
@@ -24,21 +20,20 @@ func open_close_pause():
 		how_to_play_menu.hide()
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
+# Signal callbacks
 func _on_resume_button_pressed() -> void:
-	open_close_pause()
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
+	_open_close_pause()
 
 func _on_options_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	options_menu.show()
-
-func close_optins():
-	options_menu.hide()
 	
 func _on_how_to_play_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	how_to_play_menu.show()
 
-func close_how_to_play():
-	how_to_play_menu.hide()
-
 func _on_main_menu_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	get_tree().paused = false
 	Global.restart()
