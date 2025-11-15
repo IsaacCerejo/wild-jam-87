ff0b1dd9330e71f73e49e3b7eded9520793744de
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-11-15 18:59:10 +0000

    refactor: start menu

--- scripts/start_menu/start_menu.gd
+++ scripts/start_menu/start_menu.gd
@@ -1,21 +1,25 @@
 extends Control
 
-@export var how_to_play: Control
-@export var options_menu: Control
+@onready var how_to_play_menu: Control = %HowToPlayMenu
+@onready var options_menu: Control = %OptionsMenu
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _on_start_pressed() -> void:
+func _on_start_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	get_tree().change_scene_to_file("uid://fwifwdj0klmd")
 
-func _on_options_pressed() -> void:
+func _on_options_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	options_menu.show()
 
-func _on_how_to_play_pressed() -> void:
-	how_to_play.show()
+func _on_how_to_play_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
+	how_to_play_menu.show()
 
 func _on_fullscreen_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	var mode := DisplayServer.window_get_mode()
 	
 	if mode == DisplayServer.WINDOW_MODE_FULLSCREEN or mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
@@ -24,4 +28,5 @@ func _on_fullscreen_button_pressed() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func _on_quit_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	get_tree().quit()
