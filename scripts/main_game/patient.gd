d29b919e08a7fa5892e5aa79b9fe2606b79ce428
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-11-16 18:51:32 +0000

    feat: patient manager script

--- scripts/main_game/patient.gd
+++ scripts/main_game/patient.gd
@@ -1,20 +1,35 @@
 extends Node2D
+class_name Patient
 
-@export var mushroom_count := 0
-var mushroom_scene = preload("res://scenes/main_game/mushroom.tscn")
+# Signal for when the patient is cured
+signal cured()
+
+const MUSHROOM_SCENE: PackedScene = preload(Global.SCENE_UIDS.MUSHROOM)
+
+@export var mushroom_count: int = 0
+
+var _mushrooms: Array[Mushroom] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# spawn body with random skin
-	# spawn mushrooms
-	for i in range(mushroom_count):
-		var new_mushroom := mushroom_scene.instantiate()
-		#new_mushroom.set_global_position() posição válida
+	generate_mushrooms()
 		
 	pass # Replace with function body.
 
-func cured():
-	# wait for table animation to end
-	
-	queue_free()
-	pass
+func generate_mushrooms() -> void:
+	# spawn mushrooms
+	for i in range(mushroom_count):
+		var new_mushroom: Mushroom = MUSHROOM_SCENE.instantiate() as Mushroom
+		# TODO: set Mushrooms properties like allowed_tool_types and position properly
+		new_mushroom.allowed_tool_types = [Tool.ToolType.PINCA]
+		new_mushroom.picked.connect(_on_mushroom_picked)
+		_mushrooms.append(new_mushroom)
+		add_child(new_mushroom)
+		# new_mushroom.set_global_position() posição válida
+
+
+func _on_mushroom_picked(mushroom: Mushroom) -> void:
+	_mushrooms.erase(mushroom)
+	if _mushrooms.size() == 0:
+		cured.emit()
--- scripts/main_game/patient_manager.gd
+++ scripts/main_game/patient_manager.gd
@@ -1,10 +1,29 @@
-extends Node
+extends Node2D
+class_name PatientManager
 
-@onready var operating_tray: OperatingTray = %OperatingTray
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var patient: Patient = %Patient
 
 func _ready() -> void:
-	start_round()
+	patient.cured.connect(_on_patient_cured)
+	_show_patient()
 
-func start_round():
-	# Start Timer...
-	operating_tray.spawn()
+func _show_patient() -> void:
+	patient.generate_mushrooms()
+
+	animation_player.play("slide_in")
+	# await animation_player.animation_finished
+
+	Global.time_bar.start_timer()
+
+func _on_patient_cured() -> void:
+	# Add remaining time to score
+	Global.add_time_to_score()
+
+	# Animate timer reset
+	Global.time_bar.reset_timer_animated()
+
+	animation_player.play_backwards("slide_in")
+	await animation_player.animation_finished
+
+	_show_patient()
