62fd2c88757405128af92f2f9b911fa10aaa4c91
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-11-16 16:12:56 +0000

    fix: timer script

--- scripts/main_game/time_bar.gd
+++ scripts/main_game/time_bar.gd
@@ -1,17 +1,47 @@
 extends ProgressBar
 
-var _time_left: float = 0.0
+@export var time_limit: float = 60.0:
+	set = set_time_limit
+
+var _time_left: float = 0.0:
+	set = set_time_left,
+	get = get_time_left
+
+var _finished: bool = false
 
 func _ready() -> void:
-	_time_left = max_value
-	value = _time_left
+	_time_left = time_limit
+	max_value = time_limit
+	value = 0
 
 func _process(delta: float) -> void:
+	if _finished:
+		return
+	
 	if _time_left > 0:
 		_time_left -= delta
-		value = _time_left
+		value = time_limit - _time_left
 	else:
 		_on_time_bar_finished()
 
+# Public functions
+func get_time_left() -> float:
+	return _time_left
+
+func set_time_left(new_time_left: float) -> void:
+	_time_left = clamp(new_time_left, 0.0, time_limit)
+	value = time_limit - _time_left
+
+func set_time_limit(new_time_limit: float) -> void:
+	time_limit = new_time_limit
+	max_value = time_limit
+	_time_left = max_value
+	value = 0
+	_finished = false
+
+# Signal callbacks
 func _on_time_bar_finished() -> void:
-	await Global.game_controller.change_scene(Global.SCENE_UIDS.START_MENU, "", TransitionSettings.TRANSITION_TYPE.FADE_TO_FADE)
+	if _finished:
+		return
+	_finished = true
+	await Global.game_controller.change_scene(Global.SCENE_UIDS.LOSE_SCREEN, "", TransitionSettings.TRANSITION_TYPE.FADE_TO_FADE)
