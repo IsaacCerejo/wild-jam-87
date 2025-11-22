ea0cc3f15d6cd8ab7af8333412db293d9a78ba91
PedroLlussa <115746630+PedroLlussa@users.noreply.github.com>
2025-11-22 16:15:49 +0000

    feat: Drag Body Parts

---
+++ scripts/main_game/patient_drag.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+@onready var mouse_pin: PinJoint2D = $MousePin
+@onready var mouse_body: StaticBody2D = $MousePin/MouseBody
+
+@onready var head: RigidBody2D = $"../MushroomAreas/Head"
+@onready var left_arm: RigidBody2D = $"../MushroomAreas/LeftArm"
+@onready var right_arm: RigidBody2D = $"../MushroomAreas/RightArm"
+@onready var left_leg: RigidBody2D = $"../MushroomAreas/LeftLeg"
+@onready var right_leg: RigidBody2D = $"../MushroomAreas/RightLeg"
+
+var dragging = false
+
+func _ready():
+	head.input_event.connect(_on_input_event.bind(head))
+	left_arm.input_event.connect(_on_input_event.bind(left_arm))
+	right_arm.input_event.connect(_on_input_event.bind(right_arm))
+	left_leg.input_event.connect(_on_input_event.bind(left_leg))
+	right_leg.input_event.connect(_on_input_event.bind(right_leg))
+
+func _physics_process(_delta: float) -> void:
+	mouse_pin.global_position = get_global_mouse_position()
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int, sender:RigidBody2D):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if Global.player.get_active_tool() == null:
+			dragging = true
+			print("Grabbing limb:", sender.name)
+			mouse_pin.set_node_b(sender.get_path())
+	
+		@warning_ignore("REDUNDANT_AWAIT")
+		var result: bool = await _let_go(event)
+		if result:
+			dragging = false
+			mouse_pin.set_node_b("")
+			print("Letting go of limb:", sender.name)
+	pass
+	
+# Action performed check. Function meant to be overridden.
+func _let_go(event: InputEvent) -> bool:
+	return (not event.pressed)
