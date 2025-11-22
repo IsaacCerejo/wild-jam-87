0cb71c3c481412d77825879ba13a741c3bd04f8c
IsaacCerejo <isaacerejo2@gmail.com>
2025-11-22 23:13:54 +0000

    fix: Pause bug
    Clicking Tab twice was getting an error, fixed

--- scripts/main_game/main_game_ui.gd
+++ scripts/main_game/main_game_ui.gd
@@ -7,9 +7,9 @@ class_name MainGameUI
 func _ready() -> void:
 	Global.game_ui = self
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("Pause"):
-		pause_menu._open_close_pause()
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("Pause"):
+		#pause_menu.open_close_pause()
 
 func _exit_tree() -> void:
 	Global.game_ui = null
