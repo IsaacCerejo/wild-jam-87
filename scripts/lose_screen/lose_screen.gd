2f71c612559d4e0b9e549cd48f762c454b537136
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-11-22 02:09:49 +0000

    feat: high score ui

--- scripts/lose_screen/lose_screen.gd
+++ scripts/lose_screen/lose_screen.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var mushroom_score_label: RichTextLabel = %MushroomScore
 @onready var timer_score_label: RichTextLabel = %TimerScore
 @onready var total_score_label: RichTextLabel = %TotalScore
+@onready var high_score_label: RichTextLabel = %HighScore
 
 func _ready() -> void:
 	Global.high_score = max(Global.high_score, Global.compute_total_score())
@@ -15,6 +16,9 @@ func _ready() -> void:
 
 	total_score_label.clear()
 	total_score_label.text = "[wave amp=50.0 freq=5.0 connected=1][rainbow freq=1.0 sat=0.8 val=0.8 speed=1.0]%d[/rainbow]" % Global.compute_total_score()
+
+	high_score_label.clear()
+	high_score_label.text = "[wave amp=50.0 freq=5.0 connected=1][rainbow freq=1.0 sat=0.8 val=0.8 speed=1.0]HIGHSCORE: %d[/rainbow]" % Global.high_score
 
 # Signal callbacks
 func _on_main_menu_button_pressed() -> void:
