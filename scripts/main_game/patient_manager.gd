4849bd3667809a1a42122d3ca697f73ee04234bd
Scarface1809 <joao.miguel.santos1809@gmail.com>
2025-11-22 02:10:31 +0000

    feat: mushroom count progression

--- scripts/main_game/patient_manager.gd
+++ scripts/main_game/patient_manager.gd
@@ -1,10 +1,15 @@
 extends Node2D
 class_name PatientManager
 
+# For every round_frequency rounds, increase mushroom count by mushroom_increase
+@export var round_frequency: int = 1
+@export var mushroom_increase: int = 1
+
 @onready var tray: Sprite2D = $Tray
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 var patient: Patient
+var current_round: int = 0
 
 const PATIENT_SCENE: PackedScene = preload(Global.SCENE_UIDS.PATIENT)
 const PATIENT_TUTORIAL_SCENE: PackedScene = preload(Global.SCENE_UIDS.PATIENT_TUTORIAL)
@@ -21,11 +26,17 @@ func _ready() -> void:
 
 
 func _new_patient() -> void:
+	current_round += 1
+
 	patient = PATIENT_SCENE.instantiate() as Patient
 	tray.add_child(patient)
 	patient.cured.connect(_on_patient_cured)
-	#TODO scale mushrooms by round
-	patient.mushroom_count = 12
+	
+	# Scale mushroom count based on round
+	var base_count = patient.MUSHROOM_SCENES.size() # default starting count
+	var additional = int(float(current_round) / float(round_frequency)) * mushroom_increase
+	patient.mushroom_count = base_count + additional
+
 	patient.generate_mushrooms()
 
 	animation_player.play("slide_in")
