5506f75d9b4d20ed46cf2b2fcfa2e24c67c8fc41
PedroLlussa <115746630+PedroLlussa@users.noreply.github.com>
2025-11-23 20:01:24 +0000

    feat: hairs

--- scripts/main_game/patient_skin_manager.gd
+++ scripts/main_game/patient_skin_manager.gd
@@ -5,12 +5,14 @@ extends Node2D
 @onready var left_arm_animation_player: AnimationPlayer = $"../MushroomAreas/LeftArm/LeftArmAnimationPlayer"
 @onready var right_arm_animation_player: AnimationPlayer = $"../MushroomAreas/RightArm/RightArmAnimationPlayer"
 @onready var left_leg_animation_player: AnimationPlayer = $"../MushroomAreas/LeftLeg/LeftLegAnimationPlayer"
+@onready var hair_animation_player: AnimationPlayer = $"../MushroomAreas/Head/HairRoot/HairAnimations"
 
 const head_sprites = ["Head","Head_2","Head_3","Head_4","Head_5","Head_6","Head_7"]
-const torso_sprites = ["Torso","Torso_2","Torso_3","Torso_4"]
+const torso_sprites = ["Torso","Torso_2","Torso_3","Torso_4","Torso_5"]
 const right_arm_sprites = ["RightArm","RightArm_2","RightArm_3","RightArm_4","RightArm_5"]
 const left_leg_sprites = ["LeftLeg","LeftLeg_2","LeftLeg_3","LeftLeg_4"]
 const left_arm_sprites = ["LeftArm","LeftArm_2","LeftArm_3"]
+const hair_sprites = ["Hair","Hair_2","Hair_3","Hair_4","Hair_5"]
 
 func _ready() -> void:
 	head_animPlayer.play(head_sprites.pick_random())
@@ -18,3 +20,4 @@ func _ready() -> void:
 	right_arm_animation_player.play(right_arm_sprites.pick_random())
 	left_leg_animation_player.play(left_leg_sprites.pick_random())
 	left_arm_animation_player.play(left_arm_sprites.pick_random())
+	hair_animation_player.play(hair_sprites.pick_random())
