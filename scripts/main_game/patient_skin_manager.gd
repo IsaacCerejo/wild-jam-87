aad95ba603e498452a8bda09505661cbfd60cdfb
IsaacCerejo <isaacerejo2@gmail.com>
2025-11-29 21:23:07 +0000

    feat: hair color randomizer

--- scripts/main_game/patient_skin_manager.gd
+++ scripts/main_game/patient_skin_manager.gd
@@ -9,6 +9,23 @@ extends Node2D
 @onready var left_leg_sprite: Sprite2D = $"../MushroomAreas/LeftLeg/leg_skin"
 @onready var right_leg_sprite: Sprite2D = $"../MushroomAreas/RightLeg/leg_skin"
 
+const hair_colors: Array = [
+	Color("8c4e34ff"),
+	Color("2e4a37ff"),
+	Color("2a3659ff"),
+	Color("572b44ff"),
+	Color("1d2b23ff"),
+	Color("#5A5360"),
+	Color("#5E4A45"),
+	Color("635049ff"),
+	Color("#4D5966"),
+	Color("#445248"),
+	Color("#453E4A"),
+	Color("#3A424D"),
+	Color("#2F3B32"),
+	Color("#463B36"),
+	Color("#3E3A38")
+]
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Restart"):
@@ -23,6 +40,7 @@ func _ready() -> void:
 	right_arm_sprite.texture = load_random_texture("res://assets/patient/arm")
 	left_leg_sprite.texture = load_random_texture("res://assets/patient/leg")
 	right_leg_sprite.texture = load_random_texture("res://assets/patient/leg")
+	randomize_hair_color()
 
 func load_random_texture(folder_path: String) -> Texture2D:
 	
@@ -42,3 +60,6 @@ func load_random_texture(folder_path: String) -> Texture2D:
 	var chosen_asset = assets_available.pick_random()
 
 	return load(folder_path + "/" + chosen_asset)
+
+func randomize_hair_color():
+	hair_sprite.self_modulate = hair_colors[randi_range(0, hair_colors.size()-1)]
