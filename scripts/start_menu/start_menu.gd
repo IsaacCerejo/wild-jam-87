c77c16f4beb482bc0a9467dd149f14b49ae3a3c0
IsaacCerejo <isaacerejo2@gmail.com>
2026-01-02 16:58:27 +0000

    feat: main menu polish

--- scripts/start_menu/start_menu.gd
+++ scripts/start_menu/start_menu.gd
@@ -27,15 +27,18 @@ func _on_start_button_pressed() -> void:
 	if _starting:
 		return
 	_starting = true
-
+	cycle_patient()
+	
 	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	await Global.game_controller.change_scene(Global.SCENE_UIDS.MAIN_GAME_UI, Global.SCENE_UIDS.MAIN_GAME, TransitionSettings.TRANSITION_TYPE.FADE_TO_FADE)
 
 func _on_options_button_pressed() -> void:
+	cycle_patient()
 	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	options_menu.show()
 
 func _on_how_to_play_button_pressed() -> void:
+	cycle_patient()
 	AudioManager.create_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
 	how_to_play_menu.show()
 
@@ -53,8 +56,8 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
-
-func _on_patient_button_pressed() -> void:
+	
+func cycle_patient():
 	if not can_press_patient:
 		return
 	
